aec130a7b1fc3c879a442da9d325315a9afe8d05
Ryuk <radim.maly2010@gmail.com>
2026-01-13 19:48:54 +0100

    Refactor game logic and add new enemies

--- Main.py
+++ Main.py
@@ -3,9 +3,23 @@ from enemy import Enemy
 from player import Player
 import random
 import time
+from enemy import Orc
+from enemy import Summoner
+from enemy import Bat
+from enemy import Troll
+from enemy import Goblin_king
+from enemy import Slime
+from enemy import Assasin
 
-human = Player("Aku", 40)
-orc = Enemy("Blork", 40)
+
+human = Player()
+orc = Orc()
+slime = Slime()
+troll = Troll()
+goblin_king = Goblin_king()
+bat = Bat()
+summoner = Summoner()
+assasin = Assasin()
 
 
 while True:
@@ -14,16 +28,24 @@ while True:
         print("...")
         continue
     
+    elif not question == "y":
+        print("Invalid, Please try again!")
+        print()
+
     else:
         print("Lets begin!!")
         print()
         break
 
-print("║║║║║║║║║║║║║║║║║║║║")
-print(human.info())
-print(orc.info())
-print("║║║║║║║║║║║║║║║║║║║║")
-print()
+print("║-------------------------║\n" 
+ f"║Player:                  ║\n" 
+ f"║Name: {human.name}                ║\n" 
+ f"║Player HP: {human.health}❤           ║\n" 
+ f"║                         ║\n" 
+ f"║ENEMY:                   ║\n" 
+ f"║Name: {orc.name}              ║\n" 
+ f"║Enemy HP: {orc.health}❤            ║\n" 
+"║-------------------------║\n")
 
 while True:
     print("---HUMAN ACTION---")
@@ -31,7 +53,7 @@ while True:
     print("2.use item")
     print("3.skip round")
     action = input("Select number: ")
-    
+
     if not action.isdigit():
         print("Please enter digits only!")
 
@@ -49,33 +71,47 @@ while True:
         time.sleep(2)
         print("             ║-------------------------║\n" 
                 f"             ║Player:                  ║\n" 
-                f"             ║dealt: {dmg}                 ║\n" 
-                f"             ║enemy HP: {orc.health}             ║\n" 
+                f"             ║dealt: {dmg}🩸               ║\n" 
+                f"             ║enemy HP: {orc.health}❤            ║\n" 
                "             ║-------------------------║\n")
         
         if orc.health <= 0:
+            print("Enemy defeated💀")
             print("You Win!")
             print("---------------")
             loot_item = human.get_loot()
             Player.save_loot(loot_item)
             print(f"INVENTORY: ║{human.inventory}║")
             break
+        
+        elif orc.health < 20:
+            abl = orc.ability()
+            human.take_damage(abl)
+            
+            print("Enemy is attacking...")
+            time.sleep(2)
+            print("             ║-------------------------║\n" 
+                    f"             ║ENEMY:                   ║\n" 
+                    f"             ║dealt(with ability): {abl}🩸               ║\n" 
+                    f"             ║player HP: {human.health}❤           ║\n"
+                   "             ║-------------------------║\n")
 
-        dmg = orc.deal_damage()
-        human.take_damage(dmg)
+        else:
+            dmg = orc.deal_damage()
+            human.take_damage(dmg)
         
-        print("Enemy is attacking...")
-        time.sleep(2)
+            print("Enemy is attacking...")
+            time.sleep(2)
         
-        print("             ║-------------------------║\n" 
-                f"             ║ENEMY:                   ║\n" 
-                f"             ║dealt: {dmg}                 ║\n" 
-                f"             ║player HP: {human.health}            ║\n" 
-               "             ║-------------------------║\n")
+            print("             ║-------------------------║\n" 
+                    f"             ║ENEMY:                   ║\n" 
+                    f"             ║dealt: {dmg}🩸               ║\n" 
+                    f"             ║player HP: {human.health}❤           ║\n" 
+                   "             ║-------------------------║\n")
         
-        if human.health <= 0:
-            print("You Lose!")
-            break
+            if human.health <= 0:
+                print("You Lose!")
+                break
 
     elif action == "2":
         print(Player.load_loot())
@@ -116,12 +152,12 @@ while True:
                     human.take_damage(dmg)
                     print("             ║-------------------------║\n" 
                     f"             ║Player:                  ║\n" 
-                    f"             ║dealt: {dmg}                 ║\n" 
-                    f"             ║enemy HP: {orc.health}             ║\n" 
+                    f"             ║dealt: {dmg}🩸                ║\n" 
+                    f"             ║enemy HP: {orc.health}❤            ║\n" 
                     f"             ║                         ║\n" 
                     f"             ║ENEMY:                   ║\n" 
-                    f"             ║dealt: {dmg}                 ║\n" 
-                    f"             ║player HP: {human.health}            ║\n" 
+                    f"             ║dealt: {dmg}🩸                ║\n" 
+                    f"             ║player HP: {human.health}❤           ║\n" 
                    "             ║-------------------------║\n")
 
                 elif question == "Iron sword":
@@ -129,11 +165,11 @@ while True:
                     human.take_damage(dmg)
                     print("             ║-------------------------║\n" 
                     f"             ║Player:                  ║\n" 
-                    f"             ║dealt: {dmg}                 ║\n" 
+                    f"             ║dealt: {dmg}🩸                ║\n" 
                     f"             ║enemy HP: {orc.health}             ║\n" 
                     f"             ║                         ║\n" 
                     f"             ║ENEMY:                   ║\n" 
-                    f"             ║dealt: {dmg}                 ║\n" 
+                    f"             ║dealt: {dmg}🩸                ║\n" 
                     f"             ║player HP: {human.health}            ║\n" 
                    "             ║-------------------------║\n")
             
